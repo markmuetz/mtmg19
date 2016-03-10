63380a31826012db05e695a0ad2b2f3f7440d03c
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-03-10 21:38:24 +0000

    Close to finished assessement.

--- assessed_problems.py
+++ assessed_problems.py
@@ -9,10 +9,10 @@ Rd = 287
 cpd = 1004
 L = 2.5e6
 Rv = 461.5
+g = 9.81
 
 def f(phi):
     return 2 * Omega * np.sin(np.pi /180 * phi)
-
 
 def B(phi):
     return 2 * Omega * np.cos(np.pi / 180 * phi)/a
@@ -26,14 +26,18 @@ def PE(phi):
     return np.abs((f(phi) - f_B(phi))/f(phi)) * 100
 
 def Q1():
+    print('Q1')
     plt.clf()
     test_lats = [-5, 5, 10]
 
+    print('(a)')
     for test_lat in test_lats:
         print('{}deg, f={}'.format(test_lat, f(test_lat)))
+    print('(b)')
     for test_lat in test_lats:
         print('{}deg, PE={}'.format(test_lat, PE(test_lat)))
 
+    print('(c)')
     phi = np.linspace(0, 90, 9001)
     e = PE(phi)
     e[0] = 0
@@ -47,24 +51,24 @@ def Q1():
             break
     print('phi where PE>5%: {}'.format(phiv))
     plt.show()
+    print('')
+    print('(d)')
+    # e-foldinging dist:
+    c = 80 # m/s, taken from Q3.
+    ye = np.sqrt(2*c/B(0))
+    print('ye={}'.format(ye))
+    print('ye={} deg'.format(ye/a * 180 / np.pi))
 
 def tetens(T):
     T = T - 273.15
     return 6.112 * np.exp(17.67 * T / (T + 243.5))
 
-def theta_e(theta_d, rvs, T):
-    return theta_d * np.exp(L * rvs / (cpd * T) ) * (1 - tetens(T) / 1000)**(-Rd / cpd)
-
-def theta_e_approx(theta_d, rvs, T):
-    return theta_d * np.exp(L * rvs / (cpd * T))
-
-def theta_e_approx(theta_d, rvs, T):
-    return theta_d * np.exp(L * rvs / (cpd * T))
-
-def theta_e_naive(rvs, T):
+def theta_e(rvs, T):
     return T + L * rvs / cpd
 
 def Q2():
+    print('Q2')
+    print('(a)')
     dt = 2.5
     T = 300
 
@@ -73,18 +77,58 @@ def Q2():
     pc2 = (tetens(T + dt) - tetens(T)) / tetens(T) * 100
     print('% change 2: {}'.format(pc2))
 
-    # See Ambaum p114 and https://en.wikipedia.org/wiki/Equivalent_potential_temperature
+    print('(b)')
+    # See https://en.wikipedia.org/wiki/Equivalent_potential_temperature
     # for formulae.
     for T, rvs in [(273.15 + 25, 0.017),
                    (273.15 + 40, 0.007)]:
         print('T={}, rvs={}'.format(T, rvs))
-        print('theta_e={}'.format(theta_e(T, rvs, T)))
-        print('theta_e_approx={}'.format(theta_e_approx(T, rvs, T)))
-        print('theta_e_naive={}'.format(theta_e_naive(rvs, T)))
+        print('theta_e={}'.format(theta_e(rvs, T)))
         print('')
+
+
+def Q3():
+    print('Q3')
+    print('(a)/(b)')
+    c = 80 # m/s
+    u1 = 10 # m/s
+
+    lat = 12 * np.pi / 180
+    y = lat * a
+    u2 = u1 * np.exp(-B(lat) * y**2 / (2 * c))
+     
+    for u in [u1, u2]:
+        phi = u*c
+        print('phi={} m2s-2'.format(phi))
+        Z = phi/g
+        print('Z={} m'.format(Z))
+        rho = 1.22
+        dp = -rho*g*2*Z
+        print('Delta p={} Pa, {} hPa'.format(dp, dp/100))
+        print('')
+
+
+def Q4():
+    print('Q4')
+    print('(b)')
+    for lat in [5, 10, 25]:
+        print('lat={}'.format(lat))
+        y = lat * np.pi / 180 * a
+        u = Omega * y**2 / a
+        print('u={}'.format(u))
+        print('')
+    print('(c)')
+    yc = 12 * np.pi / 180 * a
+    y = 0
+    u = Omega * (y**2 - yc**2) / a
+    print('u={}'.format(u))
+    print('')
+
 
 
 if __name__ == '__main__':
     plt.ion()
-    #Q1()
+    Q1()
     Q2()
+    Q3()
+    Q4()
